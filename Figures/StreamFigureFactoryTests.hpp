27bb0473ca1fbdae8e778e5ae4372917e1159770
Daniel Damianov <56774476+danieldamianov@users.noreply.github.com>
2023-11-22 16:47:19 +0200

    Deleted useless comment.

--- Figures/StreamFigureFactoryTests.hpp
+++ Figures/StreamFigureFactoryTests.hpp
@@ -47,42 +47,4 @@ TEST_CASE("Test stream factory")
 	CHECK(result == expectedResult);
 }
 
-/*circlePerimeterAverage /= circleCounter;
-trianglePerimeterAverage /= triangleCounter;
-rectanglePerimeterAverage /= rectangleCounter;
-
-CHECK(triangleCounter > 32000);
-CHECK(triangleCounter < 35000);
-CHECK(rectangleCounter > 32000);
-CHECK(rectangleCounter < 35000);
-CHECK(circleCounter > 32000);
-CHECK(circleCounter < 35000);
-
-double const avgFrom1To100 = 50.5;
-double expectedAverageCirclePerimeter = avgFrom1To100 * 2 * PI;
-double expectedAverageRectanglePerimeter = avgFrom1To100 * 4;
-
-double thirdSidesAverage = 0;
-
-
-for (int i = 1; i <= 100; i++)
-{
-	for (int j = 1; j <= 100; j++)
-	{
-		thirdSidesAverage += (std::max((abs(i - j) + 1), 1) + std::min((i + j - 1), 100)) / 2.0;
-	}
-}
-
-thirdSidesAverage /= 10000;
-double expectedtrianglePerimeter = thirdSidesAverage + avgFrom1To100 * 2;
-
-CHECK(circlePerimeterAverage > expectedAverageCirclePerimeter - 3);
-CHECK(circlePerimeterAverage < expectedAverageCirclePerimeter + 3);
-
-CHECK(trianglePerimeterAverage > expectedtrianglePerimeter - 3);
-CHECK(trianglePerimeterAverage < expectedtrianglePerimeter + 3);
-
-CHECK(rectanglePerimeterAverage > expectedAverageRectanglePerimeter - 3);
-*///CHECK(rectanglePerimeterAverage < expectedAverageRectanglePerimeter + 3);
-
 #endif
